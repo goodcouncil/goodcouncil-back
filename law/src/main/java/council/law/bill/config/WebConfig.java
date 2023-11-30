1da31f784fc52dc57ffba23c8606f12d2068f7e4
eunjng5474 <eunjng5474@naver.com>
2023-11-30 10:34:40 +0900

    :recycle: WebConfig 수정

--- law/src/main/java/council/law/bill/config/WebConfig.java
+++ law/src/main/java/council/law/bill/config/WebConfig.java
@@ -1,8 +1,14 @@
 package council.law.bill.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +18,20 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // 허용할 URL
+        config.addAllowedHeader("*"); // 허용할 Header
+        config.addAllowedMethod("*"); // 허용할 Http Method
+        // CORS 는 해결했지만 프론트에 응답 헤더에 추가한 Authorization 이 전달되지 않는 문제 해결
+        config.setExposedHeaders(Arrays.asList("Authorization", "refreshToken"));
+        source.registerCorsConfiguration("/**", config);
+        // 모든 Url 대해 설정한 CorsConfiguration 등록
+        return new CorsFilter(source);
     }
 }
